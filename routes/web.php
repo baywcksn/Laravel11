f60cb7e01136e9d289bca7c47e51ec2c44b8a56d
Bayuwcksn14 <bayuwcksn01@gmail.com>
2024-12-27 19:06:06 +0700

    Implement posts feature with dynamic routing and views

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,34 @@
 <?php
 
+use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
+
+class Post
+{
+    public static function all()
+    {
+        return [
+
+            [
+                'id' => '1',
+                'slug' => 'biografi-bayu',
+                'title' => 'Biografi Bayu',
+                'author' => 'Bayu Wiaksono',
+                'body' => 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minima minus inventore ex
+                    perspiciatis vero quibusdam ipsum necessitatibus rem aperiam libero! Ipsam velit esse asperiores tempora
+                    debitis quae modi quod repellat!'
+            ],
+            [
+                'id' => '2',
+                'slug' => 'biografi-santi',
+                'title' => 'Biografi Santi',
+                'author' => 'Santi Amel',
+                'body' => 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minima minus inventore ex'
+            ]
+
+        ];
+    }
+}
 
 Route::get('/', function () {
     return view('home', ['title' => 'Home Page']);
@@ -10,8 +38,35 @@ Route::get('/about', function () {
     return view('about', ['name' => 'Bayu Wicaksono', 'title' => 'About']);
 });
 
-Route::get('/blog', function () {
-    return view('blog', ['title' => 'Blog']);
+Route::get('/posts', function () {
+    return view('posts', ['title' => 'Blog', 'posts' => Post::all()]);
+});
+
+Route::get('/posts/{slug}', function ($slug) {
+    $posts =
+        [
+            [
+                'id' => '1',
+                'slug' => 'biografi-bayu',
+                'title' => 'Biografi Bayu',
+                'author' => 'Bayu Wiaksono',
+                'body' => 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minima minus inventore ex
+            perspiciatis vero quibusdam ipsum necessitatibus rem aperiam libero! Ipsam velit esse asperiores tempora
+            debitis quae modi quod repellat!'
+            ],
+            [
+                'id' => '2',
+                'slug' => 'biografi-santi',
+                'title' => 'Biografi Santi',
+                'author' => 'Santi Amel',
+                'body' => 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minima minus inventore ex'
+            ],
+        ];
+
+    $post = Arr::first($posts, function ($posts) use ($slug) {
+        return $posts['slug'] == $slug;
+    });
+    return view('post', ['title' => 'Single Post', 'post' => $post]);
 });
 
 Route::get('/contact', function () {
