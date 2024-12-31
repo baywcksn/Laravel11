e2720b544bd766c5882ff1a092fd73b3e67167da
Bayuwcksn14 <bayuwcksn01@gmail.com>
2024-12-31 13:08:08 +0700

    Refactor Post model to extend Eloquent and update post retrieval logic; enhance post view with dynamic date display

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -3,42 +3,9 @@
 namespace App\Models;
 
 use Illuminate\Support\Arr;
+use Illuminate\Database\Eloquent\Model;
 
-class Post
+class Post extends Model
 {
-    public static function all()
-    {
-        return [
-
-            [
-                'id' => '1',
-                'slug' => 'biografi-bayu',
-                'title' => 'Biografi Bayu',
-                'author' => 'Bayu Wiaksono',
-                'body' => 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minima minus inventore ex
-                    perspiciatis vero quibusdam ipsum necessitatibus rem aperiam libero! Ipsam velit esse asperiores tempora
-                    debitis quae modi quod repellat!'
-            ],
-            [
-                'id' => '2',
-                'slug' => 'biografi-santi',
-                'title' => 'Biografi Santi',
-                'author' => 'Santi Amel',
-                'body' => 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minima minus inventore ex'
-            ]
-
-        ];
-    }
-
-    public static function find($slug): array
-    {
-        $post = Arr::first(static::all(), fn($post) => $post['slug'] == $slug);
-
-
-        if (!$post) {
-            abort(404);
-        }
-
-        return $post;
-    }
+    protected $fillable = ['title', 'author', 'slug', 'body'];
 }
